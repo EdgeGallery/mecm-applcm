4acc3fe3bbb69654a58b5feae5f862971e142f76
agrawalgaurav <gaurav.agrawal@huawei.com>
2020-08-07 13:29:12 +0530

    MECM APPLCM initial API

---
+++ lcmbroker/routers/router.go
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// LCM broker APIs
+package routers
+
+import (
+	"applcm/lcmbroker/controllers"
+	"github.com/astaxie/beego"
+	)
+
+const RootPath string = "/lcmbroker/v1"
+
+func init() {
+	beego.Router(RootPath+"/app_instances/:appInstanceId/instantiate", &controllers.LcmController{},"post:Instantiate")
+	beego.Router(RootPath+"/app_instances/:appInstanceId/terminate", &controllers.LcmController{},"post:Terminate")
+	beego.Router(RootPath+"/app_instances/:appInstanceId", &controllers.LcmController{},"post:Query")
+}
