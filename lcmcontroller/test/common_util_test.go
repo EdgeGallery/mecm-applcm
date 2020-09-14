1a7a348ece69d040b118c56b8b2bf75fe0503dc3
Rama-Huawei <rama.subba.reddy.s@huawei.com>
2020-09-15 04:34:36 +0530

    Fixed the testcases failure after adding access token expiry
    Signed-off-by: Rama-Huawei <rama.subba.reddy.s@huawei.com>

--- lcmcontroller/test/common_util_test.go
+++ lcmcontroller/test/common_util_test.go
@@ -18,11 +18,13 @@ package test
 
 import (
 	"bytes"
+	"github.com/dgrijalva/jwt-go"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 var (
@@ -74,19 +76,26 @@ func getHttpRequest(uri string, params map[string]string, paramName string, path
 		req.Header.Set("Content-Type", "application/json")
 	}
 
+	accessToken := createToken(1)
 	// Add additional headers
-	req.Header.Set("access_token", "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiI3MjY5NjM4ZS01NjM3LTRiOGM"+
-		"tODE3OC1iNTExMmJhN2I2OWIiLCJzY29wZSI6WyJhbGwiXSwiZXhwIjoxNTk5Mjc5NDA3LCJzc29TZXNzaW9uSWQiOiI1QkYwNjM2QzlBMkEzMUI2NEEwM"+
-		"EFCRTk1OTVEN0E0NyIsInVzZXJOYW1lIjoid2Vuc29uIiwidXNlcklkIjoiNzI2OTYzOGUtNTYzNy00YjhjLTgxNzgtYjUxMTJiYTdiNjliIiwiYXV0aG9"+
-		"yaXRpZXMiOlsiUk9MRV9BUFBTVE9SRV9URU5BTlQiLCJST0xFX0RFVkVMT1BFUl9URU5BTlQiLCJST0xFX01FQ01fVEVOQU5UIl0sImp0aSI6IjQ5ZTBhM"+
-		"GMwLTIxZmItNDAwZC04M2MyLTI3NzIwNWQ1ZTY3MCIsImNsaWVudF9pZCI6Im1lY20tZmUiLCJlbmFibGVTbXMiOiJ0cnVlIn0.kmJbwyAxPj7OKpP-5r-"+
-		"WMVKbETpKV0kWMguMNaiNt63EhgrmfDgjmX7eqfagMYBS1sgIKZjuxFg2o-HUaO4h9iE1cLkmm0-8qV7HUSkMQThXGtUk2xljB6K9RxxZzzQNQFpgBB7gE"+
-		"cGVc_t_86tLxUU6FxXEW1h-zW4z4I_oGM9TOg7JR-ZyC8lQZTBNiYaOFHpvEubeqfQL0AFIKHeEf18Jm-Xjjw4Y3QEzB1qDMrOGh-55y8kelW1w_Vwbaz4"+
-		"5n5-U0DirDpCaa4ergleQIVF6exdjMWKtANGYU6zy48u7EYPYsykkDoIOxWYNqWSe557rNvY_3m1Ynam1QJCYUA")
+	req.Header.Set("access_token", accessToken)
 	req.Header.Set("X-Forwarded-For", fwdIp)
 
 	// Parse and create multipart form
 	_ = req.ParseMultipartForm(32 << 20)
 
 	return req, err
+}
+
+func createToken(userid uint64) string {
+	//Creating Access Token
+	atClaims := jwt.MapClaims{}
+	atClaims["authorities"] = "authorities"
+	atClaims["user_name"] = "lcmcontroller"
+	atClaims["authorized"] = true
+	atClaims["userId"] = userid
+	atClaims["exp"] = time.Now().Add(time.Minute * 60).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, _ := at.SignedString([]byte("jdnfksdmfksd"))
+	return token
 }
