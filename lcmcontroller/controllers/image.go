29ccaf28c6b07ec6b6d7ec26e7ac868f23d8f23b
agrawalgaurav <gaurav.agrawal@huawei.com>
2021-03-01 18:43:43 +0530

    Added swagger description for image management API.

--- lcmcontroller/controllers/image.go
+++ lcmcontroller/controllers/image.go
@@ -29,18 +29,56 @@ type ImageController struct {
 	Db dbAdapter.Database
 }
 
+// @Title Create Image
+// @Description creation of image
+// @Param   tenantId        path 	string	true   "tenantId"
+// @Param   appInstanceId   path 	string	true   "appInstanceId"
+// @Param   access_token    header  string  true   "access token"
+// @Param   vmId            body 	string	true   "vmId"
+// @Success 200 ok
+// @Failure 400 bad request
+// @Failure 404 vm doesn't exist
+// @router /tenants/:tenantId/app_instances/:appInstanceId/images [post]
 func (c *ImageController) CreateImage() {
 	log.Info("Image creation request received.")
 }
 
+// @Title Delete Image
+// @Description deletion of image
+// @Param   tenantId        path 	string	true   "tenantId"
+// @Param   appInstanceId   path 	string	true   "appInstanceId"
+// @Param   imageId         path 	string	true   "imageId"
+// @Param   access_token    header  string  true   "access token"
+// @Success 204 no content
+// @Failure 404 image doesn't exist
+// @router /tenants/:tenantId/app_instances/:appInstanceId/images/:imageId [delete]
 func (c *ImageController) DeleteImage() {
 	log.Info("Image deletion request received.")
 }
 
+// @Title Query Image
+// @Description query of image
+// @Param   tenantId        path 	string	true   "tenantId"
+// @Param   appInstanceId   path 	string	true   "appInstanceId"
+// @Param   imageId         path 	string	true   "imageId"
+// @Param   access_token    header  string  true   "access token"
+// @Success 200 ok
+// @Failure 404 image doesn't exist
+// @router /tenants/:tenantId/app_instances/:appInstanceId/images/:imageId [get]
 func (c *ImageController) GetImage() {
 	log.Info("Query image request received.")
 }
 
+// @Title Download Image file
+// @Description download a specific chunk of image file
+// @Param   tenantId        path 	string	true   "tenantId"
+// @Param   appInstanceId   path 	string	true   "appInstanceId"
+// @Param   imageId         path 	string	true   "imageId"
+// @Param   access_token    header  string  true   "access token"
+// @Param   chunk_num       header  string  true   "chunk number"
+// @Success 200 ok
+// @Failure 404 image or chunk doesn't exist
+// @router /tenants/:tenantId/app_instances/:appInstanceId/images/:imageId/file [get]
 func (c *ImageController) GetImageFile() {
-	log.Info("Get image file request received.")
+	log.Info("Download image file request received.")
 }
