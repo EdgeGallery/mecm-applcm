fb0498d10ef1d6a936cd7f25138ee65ef0af86fd
cuijch <cuijch@corp.21cn.com>
2021-03-02 11:17:48 +0800

    feat: 修复数据库连接

--- osplugin/core/models.py
+++ osplugin/core/models.py
@@ -6,20 +6,22 @@ from core.orm.adapter import db
 
 
 class AppInsMapper(db.Entity):
-    app_instance_id = PrimaryKey(str, 64)
-    host_ip = Required(str, 15)
-    stack_id = Required(str, 64, unique=True)
-    operational_status = Required(str, 128)
-    operation_info = Optional(str, 256, nullable=True)
+    _table_ = 't_app_instance'
+    app_instance_id = PrimaryKey(str, max_len=64)
+    host_ip = Required(str, max_len=15)
+    stack_id = Required(str, max_len=64, unique=True)
+    operational_status = Required(str, max_len=128)
+    operation_info = Optional(str, max_len=256, nullable=True)
 
 
 class VmImageInfoMapper(db.Entity):
-    app_instance_id = Required(str, 64)
-    host_ip = Required(str, 15)
-    stack_id = Required(str, 64)
-    vm_id = Required(str, 64)
-    image_id = Required(str, 64)
-    image_size = Required(int, 32)
+    _table_ = 't_vm_image_info'
+    app_instance_id = Required(str, max_len=64)
+    host_ip = Required(str, max_len=15)
+    stack_id = Required(str, max_len=64)
+    vm_id = Required(str, max_len=64)
+    image_id = Required(str, max_len=64)
+    image_size = Required(int, size=64)
 
 
 db.generate_mapping(create_tables=True)
