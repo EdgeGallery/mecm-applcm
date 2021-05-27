9c927f482d4100aa0fb6aa512b35188bc0b3d5ac
chandler-lee <13106126@163.com>
2021-05-27 10:40:11 +0800

    描述:applcm增加mep接口调用service、kong，提出公共部分到函数 解决问题:applcm增加mep接口调用service、kong

--- lcmcontroller/controllers/mep.go
+++ lcmcontroller/controllers/mep.go
@@ -30,21 +30,49 @@ type MepController struct {
 	BaseController
 }
 
+// @Title Query Services
+// @Description Query Services from Mep Service
+// @Param
+// @Success 200 ok
+// @Failure 400 bad request
+// @Failure 500 Mep Service calling failed
+// @router /mep/services [get]
 func (c *MepController) Services() {
 	log.Info("Query services request received.")
 	GetFromMep(c,util.MepServiceQuery)
 }
 
+// @Title Query Kong Logs
+// @Description Query Kong Logs from Mep Service
+// @Param
+// @Success 200 ok
+// @Failure 400 bad request
+// @Failure 500 Mep Service calling failed
+// @router /mep/kong_log [get]
 func (c *MepController) KongLog() {
 	log.Info("Query kong log request received.")
 	GetFromMep(c,util.MepKongLogQuery)
 }
 
+// @Title Query Subscribe
+// @Description Query Subscribe from Mep Service
+// @Param
+// @Success 200 ok
+// @Failure 400 bad request
+// @Failure 500 Mep Service calling failed
+// @router /mep/kong_log [get]
 func (c *MepController) Subscribe() {
 	log.Info("Query subscribe statistic request received.")
 	GetFromMep(c,util.MepSubscribeStatistic)
 }
 
+// @Title GetFromMep
+// @Description Do get from Mep Service
+// @Param c MepController
+// @Param url string
+// @Success 200 ok
+// @Failure 400 bad request
+// @Failure 500 Mep Service calling failed
 func GetFromMep(c *MepController, url string){
 	clientIp := c.Ctx.Input.IP()
 	err := util.ValidateSrcAddress(clientIp)
@@ -53,18 +81,19 @@ func GetFromMep(c *MepController, url string){
 		return
 	}
 	c.displayReceivedMsg(clientIp)
+
+	//trust all mep ca, for https calling
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-
 	client := &http.Client{Transport: tr}
-
 	response, err := client.Get(url)
 	if err != nil {
 		c.HandleLoggingForError(clientIp, util.StatusInternalServerError, util.ErrCallFromMep)
 		return
 	}
 	defer response.Body.Close()
+
 	body, err := ioutil.ReadAll(response.Body)
 	_, _ = c.Ctx.ResponseWriter.Write(body)
 	c.handleLoggingForSuccess(clientIp, "Query data from mep is successful")
