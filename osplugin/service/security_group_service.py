c9b765572d7c2ac5f33620c8cb4553d57765911b
cuijch1 <cuijch@corp.21cn.com>
2021-11-19 17:30:07 +0800

    feat: 实现安全组服务

--- osplugin/service/security_group_service.py
+++ osplugin/service/security_group_service.py
@@ -16,7 +16,17 @@
 """
 
 # -*- coding: utf-8 -*-
+import json
+
+import utils
+from core.log import logger
+from core.openstack_utils import create_neutron_client
 from internal.resourcemanager import resourcemanager_pb2_grpc
+from internal.resourcemanager.resourcemanager_pb2 import CreateSecurityGroupResponse, DeleteSecurityGroupResponse, \
+    QuerySecurityGroupResponse, CreateSecurityGroupRuleResponse, DeleteSecurityGroupRuleResponse, \
+    QuerySecurityGroupRuleResponse
+
+LOG = logger
 
 
 class SecurityGroupService(resourcemanager_pb2_grpc.SecurityGroupManagerServicer):
@@ -34,7 +44,18 @@ class SecurityGroupService(resourcemanager_pb2_grpc.SecurityGroupManagerServicer
         Returns:
 
         """
-        pass
+        LOG.info('received create security group message')
+        host_ip = utils.validate_input_params(request)
+        if host_ip is None:
+            return CreateSecurityGroupResponse(status='Failure')
+        neutron = create_neutron_client(host_ip, request.tenantId)
+
+        security_group = {
+            'name': request.securityGroup.name
+        }
+        neutron.create_security_group({'security_group': security_group})
+        LOG.info('create security group success')
+        return CreateSecurityGroupResponse(status='Success')
 
     def deleteSecurityGroup(self, request, context):
         """
@@ -46,7 +67,17 @@ class SecurityGroupService(resourcemanager_pb2_grpc.SecurityGroupManagerServicer
         Returns:
 
         """
-        pass
+        LOG.info("received delete security group message")
+        host_ip = utils.validate_input_params(request)
+        if host_ip is None:
+            return DeleteSecurityGroupResponse(status='Failure')
+
+        neutron = create_neutron_client(host_ip, request.tenantId)
+
+        neutron.delete_security_group(request.securityGroupId)
+        LOG.info("delete security group success")
+
+        return DeleteSecurityGroupResponse(status='Success')
 
     def querySecurityGroup(self, request, context):
         """
@@ -58,7 +89,24 @@ class SecurityGroupService(resourcemanager_pb2_grpc.SecurityGroupManagerServicer
         Returns:
 
         """
-        pass
+        LOG.info("received delete security group message")
+        host_ip = utils.validate_input_params(request)
+        if host_ip is None:
+            return QuerySecurityGroupResponse(response='{"code":400,"msg":"error"}')
+
+        neutron = create_neutron_client(host_ip, request.tenantId)
+
+        resp_data = {
+            'code': 200,
+            'msg': 'success'
+        }
+        if request.securityGroupId is not None:
+            resp_data['data'] = neutron.show_security_group(request.securityGroupId)
+        else:
+            resp_data['data'] = neutron.list_security_groups()
+
+        LOG.info("success query security group")
+        return QuerySecurityGroupResponse(response=json.dumps(resp_data))
 
     def createSecurityGroupRule(self, request, context):
         """
@@ -70,7 +118,28 @@ class SecurityGroupService(resourcemanager_pb2_grpc.SecurityGroupManagerServicer
         Returns:
 
         """
-        pass
+        LOG.info("received security group rule message")
+        host_ip = utils.validate_input_params(request)
+        if host_ip is None:
+            return CreateSecurityGroupRuleResponse(status='Failure')
+
+        neutron = create_neutron_client(host_ip, request.tenantId)
+
+        security_group_rule = {
+            'security_group_id': request.securityGroupRule.securityGroupId,
+            'direction': request.securityGroupRule.direction,
+            'protocol': request.securityGroupRule.protocol,
+            'ethertype': request.securityGroupRule.ethertype,
+            'port_range_max': request.securityGroupRule.portRangeMax,
+            'port_range_min': request.securityGroupRule.portRangeMin,
+            'remote_ip_prefix': request.securityGroupRule.remoteIpPrefix,
+            'remote_group_id': request.securityGroupRule.remoteGroupId
+        }
+        neutron.create_security_group_rule({'security_group_rule': security_group_rule})
+
+        LOG.info("success create security group rule")
+
+        return CreateSecurityGroupRuleResponse(status='Success')
 
     def deleteSecurityGroupRule(self, request, context):
         """
@@ -82,7 +151,18 @@ class SecurityGroupService(resourcemanager_pb2_grpc.SecurityGroupManagerServicer
         Returns:
 
         """
-        pass
+        LOG.info('received delete security group rule message')
+        host_ip = utils.validate_input_params(request)
+        if host_ip is None:
+            return DeleteSecurityGroupRuleResponse(status='Failure')
+
+        neutron = create_neutron_client(host_ip, request.tenantId)
+
+        neutron.delete_security_group_rule(request.securityGroupRuleId)
+
+        LOG.info('success delete security group rule')
+
+        return DeleteSecurityGroupRuleResponse(status='success')
 
     def querySecurityGroupRule(self, request, context):
         """
@@ -94,4 +174,21 @@ class SecurityGroupService(resourcemanager_pb2_grpc.SecurityGroupManagerServicer
         Returns:
 
         """
-        pass
+        LOG.info('received query security group rule message')
+        host_ip = utils.validate_input_params(request)
+        if host_ip is None:
+            return QuerySecurityGroupRuleResponse(response='{"code":400}')
+
+        neutron = create_neutron_client(host_ip, request.tenantId)
+
+        resp_data = {
+            'code': 200,
+            'msg': 'success'
+        }
+        if request.securityGroupRuleId is not None:
+            resp_data['data'] = neutron.show_security_group_rule(request.securityGroupRuleId)
+        else:
+            resp_data['data'] = neutron.list_security_group_rules(security_group=request.securityGroupId)
+
+        LOG.info('success query security group rule message')
+        return QuerySecurityGroupRuleResponse(response=json.dumps(resp_data))
