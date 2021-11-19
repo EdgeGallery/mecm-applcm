4ced86211e94c7022c1d3e9264921199f359845b
cuijch1 <cuijch@corp.21cn.com>
2021-11-19 17:29:51 +0800

    feat: 实现规格服务

--- osplugin/service/flavor_service.py
+++ osplugin/service/flavor_service.py
@@ -16,7 +16,15 @@
 """
 
 # -*- coding: utf-8 -*-
+import json
+
+import utils
+from core.log import logger
+from core.openstack_utils import create_nova_client
 from internal.resourcemanager import resourcemanager_pb2_grpc
+from internal.resourcemanager.resourcemanager_pb2 import CreateFlavorResponse, DeleteFlavorResponse, QueryFlavorResponse
+
+LOG = logger
 
 
 class FlavorService(resourcemanager_pb2_grpc.FlavorManagerServicer):
@@ -34,7 +42,27 @@ class FlavorService(resourcemanager_pb2_grpc.FlavorManagerServicer):
         Returns:
 
         """
-        pass
+        LOG.info('received create flavor message')
+        resp = CreateFlavorResponse(status='Failure')
+
+        host_ip = utils.validate_input_params(request)
+        if host_ip is None:
+            return resp
+
+        nova = create_nova_client(host_ip, request.tenantId)
+
+        flavor = nova.flavors.create(name=request.flavor.name,
+                                     ram=request.flavor.ram,
+                                     vcpus=request.flavor.vcpus,
+                                     disk=request.flavor.disk,
+                                     swap=request.flavor.swap)
+
+        if request.extraSpecs is not None:
+            flavor.set_keys(request.extraSpecs)
+
+        LOG.info('success create flavor %s', flavor)
+        resp.status = 'Success'
+        return resp
 
     def deleteFlavor(self, request, context):
         """
@@ -46,7 +74,19 @@ class FlavorService(resourcemanager_pb2_grpc.FlavorManagerServicer):
         Returns:
 
         """
-        pass
+        LOG.info('received delete flavor message')
+        resp = DeleteFlavorResponse(status='Failure')
+
+        host_ip = utils.validate_input_params(request)
+        if host_ip is None:
+            return resp
+
+        nova = create_nova_client(host_ip, request.tenantId)
+
+        nova.flavors.delete(request.flavorId)
+        LOG.info('success delete flavor %s', request.flavorId)
+        resp.status = 'Success'
+        return resp
 
     def queryFlavor(self, request, context):
         """
@@ -58,4 +98,25 @@ class FlavorService(resourcemanager_pb2_grpc.FlavorManagerServicer):
         Returns:
 
         """
-        pass
+        LOG.info('received query flavor message')
+        resp = QueryFlavorResponse(response='{"code": 500, "msg": "failure"}')
+
+        host_ip = utils.validate_input_params(request)
+        if host_ip is None:
+            return resp
+
+        nova = create_nova_client(host_ip, request.tenantId)
+
+        resp_data = {
+            'code': 200,
+            'msg': 'success'
+        }
+
+        if request.flavorId is not None:
+            resp_data['data'] = nova.flavors.get(request.flavorId)
+        else:
+            resp_data['data'] = nova.flavors.list()
+        resp.response = json.dumps(resp_data)
+
+        LOG.info('query flavor message success')
+        return resp
